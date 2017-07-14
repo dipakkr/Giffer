0d49d0e8355cd7effa46955018939dc51b9a731a
Deepak <raj.deeepak@gmail.com>
2017-07-15 02:32:26 +0530

    tab setuped in gif activity, all WF

--- app/src/main/java/com/dipakkr/github/giffer/activity/GifActivity.java
+++ app/src/main/java/com/dipakkr/github/giffer/activity/GifActivity.java
@@ -2,18 +2,44 @@ package com.dipakkr.github.giffer.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.dipakkr.github.giffer.R;
+import com.dipakkr.github.giffer.adapter.SimpleAdapter;
+import com.dipakkr.github.giffer.adapter.WallPaperTabAdapter;
 
 /**
  * Created by root on 7/15/17.
  */
 
 public class GifActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    TabLayout tabLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gif);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupTabs();
+
     }
+    private void setupTabs(){
+
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+        tabLayout = (TabLayout)findViewById(R.id.tabs);
+        SimpleAdapter adapter = new SimpleAdapter(getApplicationContext(),getSupportFragmentManager());
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        viewPager.setAdapter(adapter);
+
+    }
+
 }
